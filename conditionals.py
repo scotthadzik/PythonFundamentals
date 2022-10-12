83235652ff1c7ac2fee43a7da5accceb7c5d3785
scotthadzik <scotthadzik@weber.edu>
2022-10-11 18:51:58 -0600

    refactored old code

--- conditionals.py
+++ conditionals.py
@@ -40,24 +40,3 @@ while user_guess != random_number:
         number_of_guesses += 1
 
 print("Game Over you took " + str(number_of_guesses) + " guesses")   
-
-# high_range = 100
-# middle_number = int(high_range/2)
-# my_guess = middle_number
-# number_guesses = 0
-# highOrLow = "lower"
-# output = "{} is {} than {}"
-
-# my_random_number = random.randint(1, high_range)
-
-# print(my_random_number)
-# print(my_guess)
-
-# #evaluate the radom number and compare it to middle number
-# if my_guess < my_random_number:
-#     highOrLow = "lower"
-    
-# if my_guess > my_random_number :
-#     highOrLow = "higher"
-
-# print(output.format(my_guess, highOrLow, my_random_number))
